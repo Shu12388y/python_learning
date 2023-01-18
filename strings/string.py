4046b46e6a61f2e9f661ce93e5cd5c38d87bbdc9
shubham <spl.sp999@gmail.com>
2023-01-18 20:05:42 +0530

    string completed

--- strings/string.py
+++ strings/string.py
@@ -1,4 +1,46 @@
 # Here we done some string operation
 
-a="shubham paul"
-print(a)
+# a="shubham paul"
+# print(a)
+
+letter='''this is a letter and multiline string operation and I will do some of the string opertion like silcing and find th word present in the the string or not and more operators can be used to do the string operation is nad is'''
+
+# slicing operation
+# let us modify it 
+
+# n=int(input("Enter the first index: "))
+# m=int(input("Enter the last index: "))
+
+# # here skip is the skipping value
+# skip=int(input("Skip value: "))
+# sl=letter[n:skip:m]
+# print(sl)
+
+# now let us check the length of the string
+# 
+# print(len(letter))
+
+# # now let us find the string end with name or namw 
+
+# print(letter.endswith("operation"))
+
+# Now let us cound the number of particular characters
+
+# print(letter.count("t"))
+
+# Now let us capalize the number of characters
+
+# print(letter.capitalize(),"\n")
+
+# this will change the letter to uppercase
+# print(letter.upper())
+
+# Now let us find the word 
+
+# print(letter.find("is"))
+
+# let us replace a word with neword 
+
+# print(letter.replace("letter","shubham"))
+
+
